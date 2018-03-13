a91b5b95aa4d9ebe9f5e5e9b32d26a4d75a83b4d
max <kosovo@vt.edu>
2018-03-13 14:56:18 -0400

    Basic WC prediction (partialy tested)

--- cache_lru.cpp
+++ cache_lru.cpp
@@ -1,15 +1,30 @@
 #include "cache_lru.hpp"
 
 
-void simplewcount::pushNew(const addr_t& key, const int& value) {
-   pwcBuffMap.insert(std::make_pair(key, value));
-   pwcBuffQueue.push(key); 
+void simplewcount::putinCache(const int& set, const addr_t& key, const struct Cacheblock& block) {
+	
+   if (mycache[set].size() >= SET_SIZE) {
+	addr_t evictKey = mycache[set].old();
+	struct Cacheblock evictBlock = mycache[set].getOld(evictKey);
+	if (evictBlock.isDirty) {
+		evictBlock.isDirty = 0;
+		evictBlock.wcActual++;
+		//Writeback E to MM
+		mainMemory[evictKey] = evictBlock;
+	}
+   }
+   //Put A in Cache
+   mycache[set].put(key, block);
+
 }
 
-void simplewcount::popOld() {
-   addr_t key = pwcBuffQueue.front();
-   pwcBuffMap.erase(key);
-   pwcBuffQueue.pop();
+void simplewcount::updateDirty(const std::string& op, const int& set, const addr_t& key) {
+   //Update LRU
+   struct Cacheblock tempBlock = mycache[set].get(key);
+   if (op == "W") {
+   	tempBlock.isDirty = 1;
+	mycache[set].put(key, tempBlock);
+   }
 }
 
 void simplewcount::fillMainMemory(std::string filename) {
@@ -22,14 +37,25 @@ void simplewcount::fillMainMemory(std::string filename) {
    std::string fileLine;
    while (std::getline(infile, fileLine)) {
         std::istringstream traces(fileLine);
-        traces >> instrAddr >> memOp >> memAddr;  
-
+        traces >> instrAddr >> memOp >> memAddr;
+        
         cacheLine = std::stoul(memAddr, nullptr, 16) / 64;
 
-	tempBlock.wcActual = cacheLine % 10;	
 	mainMemory[cacheLine] = tempBlock;
    }
+   std::cout << "# of unique lines: " << mainMemory.size() << std::endl << std::endl;
 } 
+
+void simplewcount::printMainMemory() {
+   for (auto it : mainMemory) {
+	std::cout << it.first << "| wcA: " << it.second.wcActual << ", wcH: ";
+	for (int i = 0; i < WCHISTORY_SIZE; ++i) {
+		std::cout << it.second.wcHistory[i] << " ";
+	}
+   std::cout << std::endl;
+   }
+}
+
 
 int simplewcount::getActualwc(const addr_t& key) {
    
@@ -43,34 +69,124 @@ int simplewcount::getActualwc(const addr_t& key) {
 
 }
 
+struct simplewcount::Cacheblock simplewcount::getBlock(const addr_t& key) {
+   
+   struct Cacheblock tempBlock = {};
+   auto it = mainMemory.find(key);
+   if (it == mainMemory.end()) {
+   	throw std::range_error("NOT FOUND IN MM");		
+   }
+   else {
+	tempBlock = it->second;
+	return tempBlock;
+   }
+
+}
+
+void simplewcount::printPattern() {
+   std::cout << "Pattern buffer" << std::endl;
+   for (auto it = patternBuffQueue.cbegin(); it != patternBuffQueue.cend(); ++it) {	
+	std::cout <<  it->first << ", " << it->second << std::endl;
+   } 
+	
+}
+
+void simplewcount::updatePattern(const addr_t& key, const int& wc) {
+   //Check that patter buffer is full
+   if (patternBuffQueue.size() == PATTERNFIFO_SIZE) {
+	//Get last block
+	std::pair<addr_t, int> last = patternBuffQueue.back();
+
+	patternBuffQueue.pop_back();
+
+	//Find set
+	int set = 0;
+	if (mycache[set].exists(last.first)) {
+		struct Cacheblock tempBlock = mycache[set].getOld(last.first);
+		int i = 0;
+		for (auto it = patternBuffQueue.crbegin(); it != patternBuffQueue.crend(); ++it) {	
+			tempBlock.wcHistory[i] = it->second;
+			//TODO Ask if we update dirty status.
+			//tempBlock.isDrity = 1;
+		}
+		mycache[set].putOld(last.first, tempBlock);
+	}
+   }
+   patternBuffQueue.push_front(std::make_pair(key, wc));
+
+}
+
+
 
 
 int main() {
    using namespace simplewcount;
    
    fillMainMemory(FILE_NAME);
+   printMainMemory();
+   
+   int totalPredict = 0;
+   int correctPredict = 0;
+
 
    std::ifstream infile(FILE_NAME);
    std::string fileLine;
-  
+
    while (std::getline(infile, fileLine)) {
-        
-   	std::string instrAddr, memOp, memAddr;   
-	std::istringstream traces(fileLine);
+
+        std::string instrAddr, memOp, memAddr;
+        std::istringstream traces(fileLine);
         traces >> instrAddr >> memOp >> memAddr;
 
-	//TODO get set bits
-   	unsigned long cacheLine;
-	cacheLine = std::stoul(memAddr, nullptr, 16) / 64;
-  	int cacheSet = 0;
+        //TODO get set bits
+        unsigned long cacheLine;
+        cacheLine = std::stoul(memAddr, nullptr, 16) / 64;
+        int cacheSet = 0;
 
-	//Check if exists in cache
-	if(!mycache[cacheSet].exists(cacheLine)) {
-		int actualWc = getActualwc(cacheLine);	
-	}
+        //Check if exists in cache
+        if(!mycache[cacheSet].exists(cacheLine)) {
+	        totalPredict++;
 
-	
-	
+                int actualWc = getActualwc(cacheLine);
+                //Check that pwc buffer is not empty
+                if (!pwcBuffQueue.empty()) {
+		
+			std::cout << "pwc buffer not empty" << std::endl;
+			//Compare predicted wc and actual wc
+                        int predictStatus = 0;
+                        for (int i = 0; i <= PREDICT_RANGE; ++i) {
+                                if ((actualWc ==  pwcBuffQueue.front()) && (!predictStatus)) {
+					std::cout << "A: " << actualWc << " P: " << pwcBuffQueue.front() << std::endl;
+                                        predictStatus = 1;
+                                }
+                                pwcBuffQueue.pop();
+                        }
+			//Update coverage
+                        correctPredict = correctPredict + predictStatus;
+                }
+
+		struct Cacheblock tempBlock = getBlock(cacheLine);
+		if (memOp == "W") {
+			tempBlock.isDirty = 1;
+		}	
+		//Put block in cache	
+		putinCache(cacheSet, cacheLine, tempBlock);
+		//Put pwc buffer
+		for (int i = 0; i < WCHISTORY_SIZE; ++i) {
+			for (int j = 0; j <= PREDICT_RANGE; ++j) {
+				pwcBuffQueue.push(tempBlock.wcHistory[i] + j);
+			}
+		}	
+		//TODO pattern buffer logic
+		printPattern();
+		updatePattern(cacheLine, actualWc);
+        }
+	//Update dirty status
+	updateDirty (memOp, cacheSet, cacheLine);	
+
+	std::cout << "Correct # of predictions: " << correctPredict << std::endl;
+   	std::cout << "Total # of predictions: " << totalPredict << std::endl << std::endl;
+	printMainMemory();
    }
 
    return 0;
--- cache_lru.hpp
+++ cache_lru.hpp
@@ -15,13 +15,15 @@ namespace simplewcount {
 
 typedef unsigned long addr_t;
 
-unsigned int CACHE_SIZE = 65535;
-const int SET_NUM = 2;
+unsigned int CACHE_SIZE = 3;
+const int SET_NUM = 1;
 const unsigned int SET_SIZE = CACHE_SIZE / SET_NUM;
 
-const int WCHISTORY_SIZE = 2;
-const int PWCBUFFER_SIZE = 1000; 
+const int WCHISTORY_SIZE = 1;
 const int PATTERNFIFO_SIZE = WCHISTORY_SIZE + 1;
+
+const int PREDICT_RANGE = 2;
+const int PWCBUFFER_SIZE = WCHISTORY_SIZE * PREDICT_RANGE; 
 
 const std::string FILE_NAME = "addr_traces";
 
@@ -29,7 +31,7 @@ struct Cacheblock {
 	unsigned int data;
 	int wcActual;
 	int wcHistory[WCHISTORY_SIZE];
-	int dirtyStatus;
+	int isDirty;
 };
 
 class LruCache {
@@ -37,15 +39,22 @@ class LruCache {
 		typedef typename std::pair<addr_t, struct Cacheblock> blockPair;
 		typedef typename std::list<blockPair>::iterator listIterator;
 
-		LruCache(unsigned int maxSize) :
-			cachesize(maxSize) {
+		LruCache() :
+			cachesize(CACHE_SIZE) {
 		}
 		
 		unsigned int size() const {
 			return cacheMap.size();
 		}
 
-		void put(const addr_t& key, struct Cacheblock& value) {
+		addr_t old() {
+
+			auto last = cacheList.end();
+			last--;
+			return last->first;
+		}
+
+		void put(const addr_t& key, const struct Cacheblock& value) {
 			auto it = cacheMap.find(key);
 			cacheList.push_front(blockPair(key, value));
 			if (it != cacheMap.end()) {
@@ -62,6 +71,22 @@ class LruCache {
 			}
 		}
 		
+		void putOld(const addr_t& key, const struct Cacheblock& value) {
+			auto it = cacheMap.find(key);
+			if (it != cacheMap.end()) {
+				cacheMap.erase(it);
+				auto old = cacheList.erase(it->second);
+				
+				old = cacheList.insert(old,blockPair(key,value));
+				cacheMap[key] = old;
+			}
+			
+			if (cacheMap.size() > cachesize) {
+				throw std::range_error("CACHE OVERFLOW");
+			}
+		}
+
+		
 		bool exists(const addr_t& key) const {
 			return cacheMap.find(key) != cacheMap.end();
 		}
@@ -70,8 +95,18 @@ class LruCache {
 			auto it = cacheMap.find(key);
 			if (it == cacheMap.end()) {
 				throw std::range_error("NO SUCH KEY");
-			} else {
+			} 
+			else {
 				cacheList.splice(cacheList.begin(), cacheList, it->second);
+				return it->second->second;
+			}
+		}	
+		const struct Cacheblock& getOld(const addr_t& key) {
+			auto it = cacheMap.find(key);
+			if (it == cacheMap.end()) {
+				throw std::range_error("NO SUCH KEY");
+			} 
+			else {
 				return it->second->second;
 			}
 		}	
@@ -87,21 +122,31 @@ std::unordered_map<addr_t, Cacheblock> mainMemory;
 
 void fillMainMemory(std::string filename);
 int getActualwc(const addr_t& key);
+struct Cacheblock getBlock(const addr_t& key);
+void printMainMemory();
 
 //Cache($)
-LruCache mycache[SET_NUM](SET_SIZE);
+LruCache mycache[SET_NUM];
 
-void testCache();
+void putinCache(const int& set, const addr_t& key, const struct Cacheblock& block);
+void updateDirty(const std::string& op, const int& set, const addr_t& key);
 
 //Predicted write count buffer (pwcBuff)
-std::queue<addr_t> pwcBuffQueue; 
+std::queue<int> pwcBuffQueue; 
 std::unordered_map<addr_t, int> pwcBuffMap;
 
 void pushNew(const addr_t& key, const int& value);
 void popOld();
 
 //Pattern buffer (patterBuff)
-std::list<addr_t> patternBuffQueue; 
+std::list<std::pair<addr_t, int>> patternBuffQueue; 
+
+void updatePattern(const addr_t& key, const int& wc);
+void printPattern();
+
+//Coverage = corretPredict/totalPredict
+extern int totalPredict;
+extern int correctPredict;
 
 } 
 
